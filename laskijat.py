d245aaa4be8f3169ecfe6e908459b56121993dfa
Beuh <64888736+Beuhhhh@users.noreply.github.com>
2023-04-17 18:55:54 +0300

    added comments

--- laskijat.py
+++ laskijat.py
@@ -1,9 +1,30 @@
 class Laskija:
+    """Luokka, joka toteuttaa eri laskutoimituksia.
+    Julkiset metodit:
+        summaa(Union[int, float], Union[int, float])
+        kerro(Union[int, float], Union[int, float])
+    """
 
     def summaa(self, a, b):
+        """Palauttaa kahden luvun summan.
+        :param a: summan ensimmäinen luku
+        :type a: Union[int, float]
+        :param b: summan toinen luku
+        :type b: Union[int, float]
+        :return: lukujen a ja b summa
+        :rtype: Union[int, float]
+        """
         return sum([a, b])
 
     def kerro(self, a, b):
+        """Palauttaa kahden luvun tulon.
+        :param a: tulon ensimmäinen luku
+        :type a: Union[int, float]
+        :param b: tulon toinen luku
+        :type b: Union[int, float]
+        :return: lukujen a ja b tulo
+        :rtype: Union[int, float]
+        """
         tulo = 1
         for luku in [a, b]:
             tulo *= luku
@@ -14,9 +35,18 @@ class Laskija:
 
 class MonenLaskija(Laskija):
     def summaa(self, *args):
+        """Palauttaa x lukujen summan.
+        :return: lukujen summa
+        :rtype: Union[int, float]
+        """
         return sum(args)
 
     def kerro(self, *args):
+        """Palauttaa x lukujen tulon.
+        :return: lukujen tulo
+        :rtype: Union[int, float]
+        """
+
         tulo = 1
         for num in args:
             tulo *= num
@@ -44,4 +74,4 @@ print(ml.kerro(1, 2, 3, 4, 5, 6, 7))
 print()
 argumenttien_tulostaja(eka=42, toka='foo', kolmas=[0, 1, 2])
 print()
-argumenttien_tulostaja(nimi='Tero', ikä=41, kaupunki='Turku', oppilaitos='TAI')
+argumenttien_tulostaja(nimi='Tero', ika=41, kaupunki='Turku', oppilaitos='TAI')
